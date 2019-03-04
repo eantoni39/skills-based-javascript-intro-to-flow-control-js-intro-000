b44176816303e09266a9b49428947106676d5572
emmanuel antoni <emmanuel.antoni@blackrock.com>
2019-03-04 00:49:09 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -19,7 +19,7 @@ return (age>=13 && age <=19) ? "You are a teenager" : "You are not a teenager"
 
 function switchAge(age) {
   switch(age) {
-    case (age>=13 && age <=19):
+    case 13,14,15,16,17,18,19:
       return "You are a teenager";
       break;
     default:
